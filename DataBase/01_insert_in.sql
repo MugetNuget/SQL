719f0c3deac2ef81b7dab1a613f1b0b2b16fde90
mugetnuget <santiagocaceresv@gmail.com>
2025-05-11 17:24:02 -0500

    INSERT, UPDATE, DELETE are added

---
+++ DataBase/01_insert_in.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id, name, surname) VALUES (10, 'Maria', 'Lopez')
+
+INSERT INTO users (name, surname) VALUES ('Paco', 'Martinez')
---
+++ DataBase/02_update.sql
@@ -0,0 +1,3 @@
+UPDATE users SET age = '21' WHERE user_id = 8
+
+UPDATE users SET age = 20, init_date = '2020-6-29' WHERE user_id = 8
